99b03dc8b8310ca5fef87dab22e8fd8290e58b4c
RodolfoMorquecho <99112892+RodolfoMorquecho@users.noreply.github.com>
2022-04-30 23:56:07 -0500

    add: files

---
+++ practicaComic/TextoDistribucion.java
@@ -0,0 +1,28 @@
+package practicaComic;
+
+import imonsh.Screen;
+
+// Lo que hace
+public interface TextoDistribucion {
+    void pagPortada(Screen s);
+    void pagUno(Screen s);
+    void pagDos(Screen s);
+    void pagTres(Screen s);
+    void pagCuatro(Screen s);
+    void pagCinco(Screen s);
+    void pagSeis(Screen s);
+    void pagSiete(Screen s);
+    void pagOcho(Screen s);
+    void pagNueve(Screen s);
+    void pagDiez(Screen s);
+    void pagOnce(Screen s);
+    void pagDoce(Screen s);
+    void pagTrece(Screen s);
+    void pagCatorce(Screen s);
+    void pagQuince(Screen s);
+    void pagDieciseis(Screen s);
+    void pagDiecisiete(Screen s);
+    void pagDieciocho(Screen s);
+    void pagDiecinueve(Screen s);
+    void pagVeinte(Screen s);
+}
